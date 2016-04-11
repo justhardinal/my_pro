9019fbffb6f1bd4e5103a4561b6e4cc730844304
Hardinal Fahmi <hardinal.fahmi@gmail.com>
2016-04-11 19:23:34 +0700

    fix layout

---
+++ source-code/community/src/main/java/com/community/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.community.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author hardinal
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LayoutInterceptor()); //To change body of generated methods, choose Tools | Templates.
+    }   
+        
+}
